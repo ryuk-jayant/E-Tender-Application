b859244b03538a52182c9982b38cb81941cf0c35
adicodin <aditic.mail@gmail.com>
2024-06-06 14:15:40 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
 
 export default function App() {
   return (
-    <div className='text-red-500'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
