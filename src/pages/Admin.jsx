e5451be76a3dfebd1b7bc2a6775b417e8d638e5e
adicodin <aditic.mail@gmail.com>
2024-06-06 15:01:33 +0530

    create admin login page UI

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Admin() {
   return (
-    <div>Admin</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Admin Login</h1>
+      <form className='flex flex-col gap-4'>
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email'></input>
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password'></input>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign In</button>
+      </form>
+    </div>
   )
 }
