d0dcedc24318397980293435cb96d97c44786cd1
adicodin <aditic.mail@gmail.com>
2024-06-07 19:56:10 +0530

    complete home page UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import active_tenders from '../active_tenders.json';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
+    
+    const renderTenderCards = () => {
+        return active_tenders.map(tender => (
+          <div key={tender.id} className="bg-white shadow-md rounded px-4 py-4 mb-4 flex flex-col gap-2">
+            <h5 className="text-lg font-bold">{tender.title}</h5>
+            <p className="text-gray-600">{tender.description}</p>
+            <p className="text-gray-900">Deadline: {tender.deadline}</p>
+            <Link to={`/tender/${tender.id}`} class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded ml-auto">View More</Link>
+          </div>
+        ));
+      };
+
   return (
-    <div>Home</div>
+    <div className="max-w-xl mx-auto p-4 flex flex-col">
+      {renderTenderCards()}
+    </div>
   )
-}
+};
---
+++ src/pages/TenderDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TenderDetails() {
+  return (
+    <div>TenderDetails</div>
+  )
+}
